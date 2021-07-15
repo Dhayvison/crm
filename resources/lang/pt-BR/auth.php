6a266b444148412cbf5995ac2236b0a507073356
Dhayvison <dhayvisonbraga@hotmail.com>
2021-07-14 23:16:31 -0300

    feat: define lang to app and translate some messages

---
+++ resources/lang/pt-BR/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'E-mail ou senha incorretos.',
+    'password' => 'Senha incorreta',
+    'throttle' => 'Excesso de tentativas. Por favor, tente novamente em :seconds s',
+
+];
