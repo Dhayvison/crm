6962a044894ce411c5f45e21ecf5669552d4f8b7
Dhayvison <dhayvisonbraga@hotmail.com>
2021-07-21 11:04:48 -0300

    chore: lint Dashboard component

--- resources/js/Pages/Dashboard.js
+++ resources/js/Pages/Dashboard.js
@@ -1,21 +1,19 @@
 import Authenticated from '@/Layouts/Authenticated';
 import React from 'react';
-import { Alert } from 'bumbag'
- 
+import { Alert } from 'bumbag';
+
 export default function Dashboard(props) {
-    React.useEffect(()=>{
-        document.title = "Dashboard";
-    }, []);
-    
-    return (
-        <Authenticated
-            auth={props.auth}
-            errors={props.errors}
-            header={'Dashboard'}
-        >
-            <Alert title="Tudo certo!" type="success">
-                Você está logado.
-            </Alert>
-        </Authenticated>
-    );
+  const { auth, errors } = props;
+
+  React.useEffect(() => {
+    document.title = 'Dashboard';
+  }, []);
+
+  return (
+    <Authenticated auth={auth} errors={errors} header="Dashboard">
+      <Alert title="Tudo certo!" type="success">
+        Você está logado. Na página de Dashboard.
+      </Alert>
+    </Authenticated>
+  );
 }
