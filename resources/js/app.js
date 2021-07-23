90eea6c7dbf254de372f49dc6ef0337b48f6e56e
Dhayvison <dhayvisonbraga@hotmail.com>
2021-07-23 00:22:47 -0300

    chore: styles InertiaProgress

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,3 @@
-require('./bootstrap');
-
 // Import modules...
 import React from 'react';
 import { render } from 'react-dom';
@@ -7,16 +5,21 @@ import { App } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
 import { Provider as BumbagProvider } from 'bumbag';
 
+require('./bootstrap');
+
 const el = document.getElementById('app');
 
 render(
-    <BumbagProvider>
-      <App 
-        initialPage={JSON.parse(el.dataset.page)} 
-        resolveComponent={(name) => require(`./Pages/${name}`).default} />
-    </BumbagProvider>
-    ,
-    el
+  <BumbagProvider>
+    <App
+      initialPage={JSON.parse(el.dataset.page)}
+      resolveComponent={(name) => require(`./Pages/${name}`).default}
+    />
+  </BumbagProvider>,
+  el
 );
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({
+  color:
+    'linear-gradient(-45deg, rgba(87,79,235,1), rgba(30,103,213,1), rgba(59,130,246,1), rgba(124,58,237,1))',
+});
