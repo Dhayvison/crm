2426294a39ca3284a1622ed348ae76d90ef30104
Dhayvison <dhayvisonbraga@hotmail.com>
2021-09-17 22:13:00 -0300

    fix: clients table name on migration

--- database/migrations/2021_09_07_141007_create_clients_table.php
+++ database/migrations/2021_09_07_141007_create_clients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientTable extends Migration
+class CreateClientsTable extends Migration
 {
   /**
    * Run the migrations.
@@ -13,7 +13,7 @@ class CreateClientTable extends Migration
    */
   public function up()
   {
-    Schema::create('client', function (Blueprint $table) {
+    Schema::create('clients', function (Blueprint $table) {
       $table->id();
       $table->string('name');
       $table->timestamp('birth_date');
@@ -35,6 +35,6 @@ class CreateClientTable extends Migration
    */
   public function down()
   {
-    Schema::dropIfExists('client');
+    Schema::dropIfExists('clients');
   }
 }
