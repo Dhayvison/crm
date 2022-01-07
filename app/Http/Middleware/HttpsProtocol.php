c684f3aba4e1a3f58610ead23455420f38684073
Dhayvison <dhayvisonbraga@hotmail.com>
2022-01-07 08:21:38 -0300

    fix: HttpsProtocol namespace

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace MyApp\Http\Middleware;
+namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
 class HttpsProtocol
 {
 
-  public function handle($request, Closure $next)
+  public function handle(Request $request, Closure $next)
   {
     if (!$request->secure() && App::environment() === 'production') {
       return redirect()->secure($request->getRequestUri(), 301);
