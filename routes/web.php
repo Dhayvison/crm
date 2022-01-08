3c4269d0b82453bacbd7664d2a8a656a40574291
Dhayvison <dhayvisonbraga@hotmail.com>
2022-01-08 16:20:57 -0300

    fix(routes): add home route

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ use App\Http\Controllers\ClientsController;
 |
 */
 
+Route::get('/', function () {
+    return redirect(route('dashboard'));
+})->name('home');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
