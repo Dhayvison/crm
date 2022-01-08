68584c2e9724c958f29f7648e651383af744e23f
Dhayvison <dhayvisonbraga@hotmail.com>
2022-01-08 15:32:03 -0300

    fix: env variable

--- resources/js/Utils/navigation.js
+++ resources/js/Utils/navigation.js
@@ -3,7 +3,7 @@
  * @returns {string} endpoint named by param routeName
  */
 function endpoint(routeName, params) {
-  if (process.env.APP_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     return route(routeName, params, false);
   }
   return route(routeName, params);
